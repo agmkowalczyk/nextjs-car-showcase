adfab307b11f2fc22607d9b63a48be0fb74ae785
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-12-18 20:48:45 +0100

    Add Hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import { Hero } from '@/components'
 
-export default function Home() {
+const Home = () => {
   return (
-    <main className="overflow-hidden">
-      
+    <main className='overflow-hidden'>
+      <Hero />
     </main>
   )
 }
+
+export default Home
